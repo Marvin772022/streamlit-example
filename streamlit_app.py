af32356650715778b04f22961f82f90a1699cf5c
Marvin772022 <147940548+Marvin772022@users.noreply.github.com>
2023-10-14 19:28:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,18 +5,7 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-df = pd.DataFrame(np.random.randn(10, 10), columns=("col %d" % i for i in range(5)))
+df = pd.DataFrame(np.random.randn(10, 5), columns=("col %d" % i for i in range(5)))
 
 st.table(df)
 
